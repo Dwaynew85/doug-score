3cd8def6e52067936e12e6381245fbe8dd196099
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-04-22 02:48:38 -0400

    added useEffect to handle API load

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import config from "../config";
 
 function CarList() {
+
+    useEffect(() => {
+        window.gapi.load("client", this.initClient);
+    }, [])
+
+    initClient = () => {
+        window.gapi.client
+            .init({
+                apiKey: config.apiKey,
+                discoveryDocs: config.discoveryDocs
+            })
+            .then(() => {
+                load(this.onLoad);
+            });
+    };
+
     return (
         <div>
             This will be the car list
